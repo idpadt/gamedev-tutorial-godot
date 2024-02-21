c4e8465f8ba28c5782b623ee78ded561329840f1
Aditya <idpadityarahman@gmail.com>
2024-02-21 21:08:11 +0700

    hehe

--- scenes/GrassEater.gd
+++ scenes/GrassEater.gd
@@ -1,12 +1,20 @@
-extends Node2D
+extends RigidBody2D
 
-export var speed = 500.0
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var speed = 500
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Platform initialized")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 func _process(delta):
 	if Input.is_action_pressed("move_up"):
 		move_local_y(-speed * delta)
--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -3,3 +3,6 @@ extends Area2D
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 	if (body.name == "BlueShip"):
 		print("Reached objective!")
+	
+	
+	get_tree().change_scene("res://scenes/Level 2.tscn")
